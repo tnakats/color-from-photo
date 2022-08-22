3a92900bf61b1965f9a68c08bb5f9e810f8b305b
Tyler Nakata <naka8177@pacificu.edu>
2022-08-22 09:38:51 -1000

    Working input selection with button

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## color-from-photo
+
+https://thewebdev.info/2021/11/20/how-to-display-a-image-selected-from-file-input-in-react/
+
+https://www.npmjs.com/package/react-image-file-resizer
--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Resizer from 'react-image-file-resizer';
+
 
 function App() {
+  const [img, setImg] = useState();
+
+  const onUpload= (e) => {
+    const [file] = e.target.files;
+    setImg(URL.createObjectURL(file));
+  };
+  
+  const resizeFile = (file) =>
+    new Promise((resolve) => {
+      Resizer.imageFileResizer(
+        file,
+        300,
+        300,
+        "JPEG",
+        10,
+        0,
+        (uri) => {
+          resolve (uri);
+        },
+        "base64"
+      );
+    });
+
+  const onFileResize = async (e) => {
+    try {
+      const file = e.target.files[0];
+      const image = await resizeFile(file);
+      console.log(image);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <input 
+          type="file" 
+          accept="image/*"
+          id="fileSelect"
+          onChange={onUpload}
+          hidden
+          />
+
+        <img className="photo" src={img} alt=""/>
+        
+         <label for="fileSelect">
+          Add Image
+        </label>
+        <img className="colorPallette" src={img} alt="" onChange={onFileResize}/>
     </div>
+
+    
   );
 }
+
 
 export default App;
